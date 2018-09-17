5bd7e02be7dacb5b3cadeb479d44bfe7576836f5
hzjqn <hzdes.95@gmail.com>
2018-09-17 13:07:07 -0300

    updated login

--- login.php
+++ login.php
@@ -1,15 +1,51 @@
 <?php
+    // Components
+    require_once './components/dataValidationComponent.php';
+    require_once './components/userComponent.php';
+
+    session_start();
+    
+    if(isset($_SESSION['user'])){
+        header('Location: ./index.php');
+        exit;
+    }
+
     $tituloPagina = "Ágora: Explora. Expresa. Se parte"; // Esta variable cambia el titulo de la pagina
+    if($_POST){    
+        if((isset($_POST['username']) && $_POST['username'] != null) && (isset($_POST['password']) && $_POST['username'] != null)){
+            
+            var_dump($_POST);
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $errors = validateLogin($username, $password);
+        
+            if(!$errors){
+                $user = getUserData($username);
+                $_SESSION['user'] = $user;
+            }
+            
+            if(isset($_SESSION['user'])){
+            header('Location: ./index.php');
+            exit;
+            }
+
+        } else {
+            $errors['username'] = 'Debes completar este campo';
+        }
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php        
-        include("./header.php"); // Incluimos el header (Tags html y head, incluyendo los archivos css) ver "header.php"
+        require_once("./header.php"); // Incluimos el header (Tags html y head, incluyendo los archivos css) ver "header.php"
     ?>
 </head>
 <body>
-    <?php include('./navbar.php'); ?> <!-- INCLUIMOS EL NAVBAR -->
+    <?php require_once('./navbar.php'); ?> <!-- INCLUIMOS EL NAVBAR -->
     
     <!-- MAIN -->
     <main>
@@ -17,6 +53,9 @@
         <section id="login">
             <!-- FORMULARIO DE INICIO DE SESIÓN -->
             <form method="post" id="login-form">
+                <?php if(isset($errors)){foreach($errors as $value){
+                    echo $value;
+                }} ?>
                 <input type="text" name="username" placeholder="Nombre de usuario o dirección de correo electronico">
                 <input type="password" name="password" placeholder="Contraseña">
                 <input type="checkbox" name="remember" value="true"><label for="remember">Recordar</label>
@@ -30,8 +69,8 @@
     <!-- TERMINA MAIN -->
 
     <?php
-    include("./footer.php"); 
-    include("./js.php");
+    require_once("./footer.php"); 
+    require_once("./js.php");
     ?>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,6 +1,14 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-agora">
     <div class="container">
     <a class="navbar-brand" href="./index.php"><h1 class="logo"><i class="agora-logo">ágora</i></h1></a>
+    
+    <?php        
+        if(isset($_POST['logout']) && $_POST['logout'] === 'true'){
+            session_destroy();
+            header("Refresh:0");
+        }
+
+        if(!isset($_SESSION['user'])){?>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <i class="icon-bar"></i>
         <i class="icon-bar"></i>
@@ -18,6 +26,11 @@
                 <a class="nav-link reg" href="./register.php">Registrarse</a>
             </li>
         </ul>
+        <?php } else {?>
+        <div class="ml-auto">
+            <h2><?=$_SESSION['user']['username']?></h2><img class="navbar-pp" src="<?= $_SESSION['user']['pp']."pp.png" ?>" alt=""><form method="post"><button type="submit" name="logout" value="true" ><i class="fas fa-sign-out-alt"></i></button></form>
+        </div>
+        <?php } ?>
     </div>
     </div>
 </nav>
